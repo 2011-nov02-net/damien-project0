8ff63606444ece95cb501db86ba7d88d4ecebc98
Damien Bevins <damien.bevins@outlook.com>
2020-11-17 21:00:52 -0500

    Forgot to include this in the last commit

---
+++ StoreManager/StoreManager.Library/Factory/ISEntityFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using StoreManager.Library.Data;
+using StoreManager.Library.Entity;
+
+namespace StoreManager.Library.Factory
+{
+    internal interface IFactory { }
+
+    internal interface ISEntityFactory<T> : IFactory
+        where T : SEntity
+    {
+        List<T> Items { get; set; }
+
+        int Create(IData data);
+        T Get(int id);
+        void Update(int id, IData data);
+        void Delete(int id);
+    }
+}
